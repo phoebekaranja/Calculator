9583cd91217a80860befaefb0626f4c786b4823c
phoebekaranja <phoebemurish@gmail.com>
2018-08-08 18:10:51 +0300

    add initial js/scripts

---
+++ script.js
@@ -0,0 +1,28 @@
+var add = function(number1, number2) {
+  return number1 + number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(add(number1, number2));
+var add  = function (number1, number2) {
+return number1 + number2;
+};
+var subtract = function(number1, number2) {
+return number1 - number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(subtract(number1, number2));
+var multiplication = function(number1, number2) {
+return number1 * number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+var division = function(number1, number2) {
+return number1 / number2;
+};
+var number1 = parseInt(prompt("Enter a number:"));
+var number2 = parseInt(prompt("Enter another number:"));
+alert(division(number1, number2));
+var division = function(number1, number2) {
+return number1 / number2;
